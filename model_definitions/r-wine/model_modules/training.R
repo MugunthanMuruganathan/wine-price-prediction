5a46e5ad3106f53906f7f0586405b4d8f42073f9
MugunthanMuruganathan <mugunthan.muruganathan@gmail.com>
2025-08-26 12:36:33 +0530

    Adding more comments

--- model_definitions/r-wine/model_modules/training.R
+++ model_definitions/r-wine/model_modules/training.R
@@ -14,10 +14,14 @@ train <- function(data_conf, model_conf, ...) {
 
     table <- tbl(con, sql(data_conf$sql))
 
+	print data_conf$featureNames 
+	print (" ...... Next parameter ")
+	print data_conf$targetNames 
+	
     # Create dataframe from tibble, selecting the necessary columns and mutating integer64 to integers
     # select both the feature and target columns (ignorning e.g. entity key)
     columns <- unlist(c(data_conf$featureNames, data_conf$targetNames), use.name = TRUE)
-	print columns 
+	
     data <- table %>% select(all_of(columns)) %>% mutate(
                        YearId = as.integer(YearId),
                        WinterRain = as.integer(WinterRain),
